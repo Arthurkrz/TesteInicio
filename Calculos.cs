4b9da9267603869aec9db18c03efa53beb344632
Arthur K <arthurk031@outlook.com>
2024-11-14 20:07:28 -0300

    Início

---
+++ Calculos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    public class Calculos
+    {
+        public int Soma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public int Subtracao(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+        public int Multiplicacao(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+        public double Divisao(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
